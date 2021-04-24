740f923db0f4c899b16f066ccf491f3f6096c8a2
myhrelin <77819233+myhrelin@users.noreply.github.com>
2021-04-24 12:44:07 +0700

    Add files via upload

---
+++ 300x250.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '5f6b2fd1651e36f05e979f1fa2683881',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.bestdisplayformats.com/5f6b2fd1651e36f05e979f1fa2683881/invoke.js"></scr' + 'ipt>');
