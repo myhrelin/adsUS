115569bb2d136458dd0a19375591d65d891b5858
myhrelin <77819233+myhrelin@users.noreply.github.com>
2022-11-15 17:03:53 +0700

    Update 300x250.js

--- 300x250.js
+++ 300x250.js
@@ -1,8 +1,8 @@
 atOptions = {
-	'key' : '5f6b2fd1651e36f05e979f1fa2683881',
+	'key' : 'fdf3be4e6f42068d3af6cb8898a9d79c',
 	'format' : 'iframe',
 	'height' : 250,
 	'width' : 300,
 	'params' : {}
 };
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://7fva8algp45k.com/5f6b2fd1651e36f05e979f1fa2683881/invoke.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/fdf3be4e6f42068d3af6cb8898a9d79c/invoke.js"></scr' + 'ipt>');
