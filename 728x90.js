9f59d8c7a6aab261959c73e0a9640f1a8c92f463
myhrelin <77819233+myhrelin@users.noreply.github.com>
2025-01-21 18:40:47 +0700

    Create 728x90.js

---
+++ 728x90.js
@@ -0,0 +1,8 @@
+atOptions = {
+	'key' : '900045800ed25de01876cf75da399eb2',
+	'format' : 'iframe',
+	'height' : 90,
+	'width' : 728,
+	'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/900045800ed25de01876cf75da399eb2/invoke.js"></scr' + 'ipt>');
